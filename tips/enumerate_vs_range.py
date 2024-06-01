5691de9d5729b4592e376f5642e4a05266d0246d
Warahena Liyanage Lasantha Lakmal <lasanthaslakmal@gmail.com>
2024-06-01 16:09:57 +0100

    Added enumerate vs range tip

---
+++ tips/enumerate_vs_range.py
@@ -0,0 +1,14 @@
+sequence = [ "A", "Z", "C", "F", "D", "B"]
+
+# Traditional way
+print('### Traditional way ###')
+for i in range(len(sequence)):
+    value = sequence[i]
+    print(i, value)
+
+print('\n')
+
+# Using enumerate
+print('### Using enumerate ###')
+for i, value in enumerate(sequence):
+    print(i, value)
